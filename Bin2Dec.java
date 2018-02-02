2855675b0bd59c9b87e797415faf9050e8edc426
bandofpv <andobernas@gmail.com>
2018-02-02 09:43:32 -0800

    Add files via upload

---
+++ Bin2Dec.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Bin2Dec{
+
+	public static int BinaryToDecimal(int binaryNumber){
+
+		int decimal = 0;
+		int p = 0;
+		while(true){
+			if(binaryNumber == 0){
+				break;
+			} else {
+				int temp = binaryNumber % 10;
+				decimal += temp * Math.pow(2, p);
+				binaryNumber = binaryNumber/10;
+				p++;
+			}
+		}
+		return decimal;
+	}
+
+	public static void main(String args[]){
+		int binStr;    // The Input binary string
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a Binary number: ");
+		binStr = in.nextInt();   // use next() to read a String
+
+		int ret = BinaryToDecimal(binStr);
+
+		System.out.println("The equivalent decimal number for binary " + binStr + " is: " + ret);
+	}
+}
--- TestPalindromicWord.java
+++ TestPalindromicWord.java
@@ -1,29 +1,40 @@
 import java.util.Scanner;
 
 public class TestPalindromicWord {
-
 	public static void main(String[] args) {
 		String inStr;        // input String
 		int inStrLen;
+		int count = 0;
+
 
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter a String: ");
 		inStr = in.next();   // use next() to read a String
 		inStr = inStr.toLowerCase();
 		inStrLen = inStr.length();
-		
+
 		int fIdx = 0, bIdx = inStrLen -1;
 		while (fIdx < bIdx) {
 			char f = inStr.charAt(fIdx);
 			char e = inStr.charAt(bIdx);
-			
+
 			if (f == e) {
 				fIdx++;
 				bIdx--;
+				count = 1;
 			}
 			else {
+				count = 0;
 				break;
 			}
+		}
+
+		if(count == 1) {
+			System.out.print(inStr + " is a palindromic");
+		}
+		
+		else {
+			System.out.print(inStr + " is not palindromic");
 		}
 
 	}
