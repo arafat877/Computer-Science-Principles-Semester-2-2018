bdff14426ce966e640444d986c447baa40e4bbd7
bandofpv <andobernas@gmail.com>
2018-04-11 09:40:32 -0700

    Add files via upload

--- MyDate.java
+++ MyDate.java
@@ -1,4 +1,24 @@
 
 public class MyDate {
+	private int year;
+	private int month;
+	private int day;
+	
+	private String[] strMonths = new String[] {"Jan", "Feb", "Mar", "Apr", "Aug", "Sept", "Oct", "Nov", "Dec"};
+	private String[] strDays = new String[] {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+	private int[] daysInMonths = new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31,30, 31};
+	
+	public static boolean isLeapYear(int year) {
+		if ((year % 4 == 0) && (year % 100 != 0)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static boolean  isValidDate(int year, int month, int day) {
+		
+	}
 
 }
